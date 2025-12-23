e2ed922c1e1a1e71e588eb1a08ea8bdf27af5e20
blckbird0 <tibielmoreira@gmail.com>
2025-12-23 11:41:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // AFFILIATE LINK
 const AFFILIATE_URL =
-  "https://mnbxs220ervljq8-mx4-berzeo.health-line.me/azVAaudELz/MNBXs220eRVLJQ8/?al=105073&ap=-1&esub=-7EBRQCgQAAAfGpAQDX5sDma83cZoB-wcAAw_-90ZpEQ0aEQ0aEQ1CEQ1aA0JSA3Vzf2FkY29tYm__dUFzUldHYjkAA2VX&site_option=0&target=-7EBNQCgQAAAfGpAQDX5sABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx";
+  "https://uh8f8d208auh.axdsz.pro/?rid=-7EBNQCgQAAHCwWwYABgEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwExhttps://mnbxs220ervljq8-mx4-berzeo.health-line.me/azVAaudELz/MNBXs220eRVLJQ8/?al=105073&ap=-1&esub=-7EBRQCgQAAAfGpAQDX5sDma83cZoB-wcAAw_-90ZpEQ0aEQ0aEQ1CEQ1aA0JSA3Vzf2FkY29tYm__dUFzUldHYjkAA2VX&site_option=0&target=-7EBNQCgQAAAfGpAQDX5sABQEBEREKEQkKEQ1CEQ0SAAF_YWRjb21ibwEx";
 
 // CTA redirect
 function goToAffiliate() {
